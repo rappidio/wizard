d73175df8ad0c21226fe77062321ddf82ea9ab81
Marcus Krejpowicz <krebbl@gmail.com>
2013-10-16 18:32:45 +0200

    initial commit

---
+++ public/app/WizardClass.js
@@ -0,0 +1,25 @@
+define(
+    ["js/core/Application"],
+    function (Application) {
+
+        return Application.inherit({
+            /**
+             *  initializes the application variables
+             */
+            initialize:function () {
+                this.set('appName','Wizard');
+            },
+            /***
+             * Starts the application
+             * @param parameter
+             * @param callback
+             */
+            start:function (parameter, callback) {
+                // false - disables autostart
+                this.callBase(parameter, false);
+
+                callback();
+            }
+        });
+    }
+);
