2843589ecd041bc85a56d9cb4d646690a2bfb816
Marcus Krejpowicz <krebbl@gmail.com>
2013-10-17 12:16:17 +0200

    added mac address radio group

--- public/app/WizardClass.js
+++ public/app/WizardClass.js
@@ -1,26 +1,47 @@
-define(
-    ["js/core/Application", "app/model/FirewallConfiguration"],
+define(["js/core/Application", "app/model/FirewallConfiguration"],
     function (Application, FirewallConfiguration) {
 
         return Application.inherit({
             defaults: {
-                configuration: FirewallConfiguration
+                /***
+                 * @codeBehind
+                 * @type js.core.I18n
+                 */
+                i18n: null,
 
+
+                locales: null,
+                configuration: FirewallConfiguration
             },
+
+//            _commitChangedAttributes: function ($) {
+//                console.log($);
+//                this.callBase();
+//
+//            },
+
             /***
              * Starts the application
              * @param parameter
              * @param callback
              */
-            start:function (parameter, callback) {
+            start: function (parameter, callback) {
+
+                this.set('locales', ["de", "en"]);
+
                 // false - disables autostart
                 this.callBase(parameter, false);
 
                 callback();
             },
 
-            isWanConfigurationActive: function(type) {
-              return this.get('configuration.wan.type') === type;
+            /***
+             * checks if the "type" wan configuration is active
+             * @param type - the type to check
+             * @returns {boolean}
+             */
+            isWanConfigurationActive: function (type) {
+                return this.get("configuration.wan.type") === type;
             }.onChange("configuration.wan")
         });
     }
--- public/app/entity/WanConfiguration.js
+++ public/app/entity/WanConfiguration.js
@@ -1,22 +1,34 @@
 define(['js/data/Entity'], function (Entity) {
 
-  return Entity.inherit('app.entity.WanConfiguration', {
+    return Entity.inherit('app.entity.WanConfiguration', {
 
-    schema: {
-      enableNat: Boolean,
-      type: String,
-      macAddress: {required: false, type: String},
-      'interface': String,
-      dnsServer1: String,
-      dnsServer2: String,
-      dnsServer3: String
-    },
-    defaults: {
-      enableNat: true,
-      type: null,
-      'interface': 'eth0'
-    }
+        schema: {
+            enableNat: Boolean,
+            type: String,
+            macAddress: {
+                required: function () {
+                    return this.$.overwriteMacAddress;
+                },
+                type: String
+            },
+            'interface': String,
+            dnsServer1: String,
+            dnsServer2: String,
+            dnsServer3: String
+        },
+        defaults: {
+            enableNat: true,
+            type: null,
+            'interface': 'eth0',
+            overwriteMacAddress: false
+        },
+        _commitOverwriteMacAddress: function (value) {
+            // clear mac address from wan configuration
+            if (!value) {
+                this.set('macAddress', null);
+            }
+        }
 
-  });
+    });
 
 });
