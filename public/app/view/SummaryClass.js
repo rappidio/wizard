e1dd836b26b66710ecc2673d035a104140bde467
Michael Quaas <michael.quaas@adytonsystems.com>
2013-10-25 21:41:58 +0200

    Summary

--- public/app/view/SummaryClass.js
+++ public/app/view/SummaryClass.js
@@ -7,6 +7,18 @@ define(["js/ui/View", "js/core/I18n"], function (View, I18n) {
 
     defaults: {
       configuration: null
-    }
+    },
+
+    labelNat: function(){
+      return this.get('configuration.wan.enableNat') ? 'yes' : 'no';
+    }.onChange('configuration.wan.enableNat'),
+
+    labelDnsServer: function(){
+      var dnsServers, dns2, dns3;
+      dnsServers = [this.get('configuration.wan.dnsServer1')]
+      if (dns2 = this.get('configuration.wan.dnsServer2')) dnsServers.push(dns2);
+      if (dns3 = this.get('configuration.wan.dnsServer3')) dnsServers.push(dns3);
+      return this.get('configuration.wan.useDhcpDns') ? 'over DHCP' : dnsServers.join(', ');
+    }.onChange('configuration.wan.useDhcpDns', 'configuration.wan.dnsServer1', 'configuration.wan.dnsServer2', 'configuration.wan.dnsServer3')
   });
 });
